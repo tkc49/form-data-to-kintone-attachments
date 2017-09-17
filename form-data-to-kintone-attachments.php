d5d965c8811e0e5fa62c96060b5ebe8fb0f36cbf
tkc49 <h.tkc49@gmail.com>
2017-09-17 14:53:17 +0900

    first commit

---
+++ form-data-to-kintone-attachments.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Plugin Name: Form data to kintone Attachments.
+ * Plugin URI:  
+ * Description: This plugin is an addon for "kintone form".
+ * Version:	 1.0.0
+ * Author:	  Takashi Hosoya
+ * Author URI:  http://ht79.info/
+ * License:	 GPLv2 
+ * Text Domain: kintone-form-attachments
+ * Domain Path: /languages
+ */
+
+/**
+ * Copyright (c) 2017 Takashi Hosoya ( http://ht79.info/ )
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+define( 'KINTONE_FORM_ATTACHMENTS_URL',  plugins_url( '', __FILE__ ) );
+define( 'KINTONE_FORM_ATTACHMENTS_PATH', dirname( __FILE__ ) );
+
+
+$KintoneFormAttachments = new KintoneFormAttachments();
+$KintoneFormAttachments->register();
+
+require KINTONE_FORM_ATTACHMENTS_PATH . '/modules/file.php';
+
+
+
+class KintoneFormAttachments {
+
+	private $version = '';
+	private $langs   = '';
+	private $nonce   = 'kintone_form_attachments_';
+		
+	function __construct()
+	{
+		$data = get_file_data(
+			__FILE__,
+			array( 'ver' => 'Version', 'langs' => 'Domain Path' )
+		);
+		$this->version = $data['ver'];
+		$this->langs   = $data['langs'];
+		
+	}
+
+	public function register()
+	{
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ), 1 );
+	}
+
+	public function plugins_loaded()
+	{
+		load_plugin_textdomain(
+			'kintone-form-attachments',
+			false,
+			dirname( plugin_basename( __FILE__ ) ).$this->langs
+		);
+
+		add_filter( 'kintone_form_attachments_data', array( $this, 'kintone_form_attachments_data' ), 10, 6 );
+		add_filter( 'kintone_fieldcode_supported_list', array( $this, 'kintone_fieldcode_supported_list' ), 10, 1 );
+
+		
+	}
+
+	public function kintone_form_attachments_data( $kintone_setting_data, $appdata, $cf7_send_data, $kintone_form_data, $cf7_mail_tag, $e ){
+
+		return KintoneForm_file::get_kintone_file_key( $kintone_setting_data, $appdata, $cf7_send_data, $kintone_form_data, $cf7_mail_tag, $e );
+
+	}
+
+	public function kintone_fieldcode_supported_list( $kintone_fieldcode_supported_list ){
+
+		$kintone_fieldcode_supported_list['FILE'] = 'file';
+
+		return $kintone_fieldcode_supported_list;
+
+	}
+}
---
+++ modules/file.php
@@ -0,0 +1,99 @@
+<?php 
+
+class KintoneForm_file
+{
+
+	/*
+	 * get instance
+	 */
+	public static function getInstance()
+	{
+		/**
+		* a variable that keeps the sole instance.
+		*/
+		static $instance;
+
+		if ( !isset( $instance ) ) {
+			$instance = new KintoneForm_file();
+		}
+		return $instance;
+	}
+
+	public static function get_kintone_file_key( $kintone_setting_data, $appdata, $cf7_send_data, $kintone_form_data, $cf7_mail_tag, $e ) {
+
+		$return_data = array();
+
+		$value = $cf7_send_data[$cf7_mail_tag];
+
+		if( $kintone_form_data['required'] == 'true' && empty($value) ){
+			$e->add('Error', $cf7_mail_tag .'->'. $kintone_form_data['code'].' : Required fields');
+		}		
+
+		$submission = WPCF7_Submission::get_instance();
+		if ( empty( $submission ) ) {
+		    return;
+		}		
+		$uploaded_files = $submission->uploaded_files();
+
+		if(!isset($uploaded_files[$cf7_mail_tag])){
+			return;
+		}
+
+		$filePath = $uploaded_files[$cf7_mail_tag];
+		$fileName = mb_convert_encoding(mb_substr($filePath, mb_strrpos($filePath, DIRECTORY_SEPARATOR) + 1), "UTF-8", "auto");
+		$fileSize = filesize( $filePath );
+
+		$finfo = finfo_open(FILEINFO_MIME_TYPE);  
+		$mimeType = finfo_file($finfo, $filePath);
+		$file_data = file_get_contents($filePath);
+		finfo_close($finfo);  
+		
+		$request_url = 'https://'.$kintone_setting_data['domain'].'/k/v1/file.json';
+		$headers = array( 
+			'X-Cybozu-API-Token' 	=> $appdata['token'] ,
+		);
+
+		$boundary = '----'.microtime(true);
+		$body = '--'.$boundary."\r\n".
+		        'Content-Disposition: form-data; name="file"; filename="'.$fileName.'"'."\r\n".
+		        'Content-Type: '.$mimeType."\r\n\r\n".
+		        $file_data."\r\n".
+		        '--'.$boundary.'--';
+
+		$res = wp_remote_post( 
+			$request_url, 
+			[
+		    	'headers' => [
+		    		'Content-Type' => "multipart/form-data; boundary={$boundary}", 
+		    		'X-Cybozu-API-Token' 	=> $appdata['token'],
+		    		'Content-Length' 	=> strlen($body),
+		    	],
+		    	'body' => $body
+			]
+		);
+
+		if ( is_wp_error( $res ) ) {
+
+			$e = $res;
+			return $res;
+
+		} else {
+
+			$return_value = json_decode( $res['body'], true );
+			$return_data['value'][] = $return_value;
+			
+			if ( isset( $return_value['message'] ) && isset( $return_value['code'] ) ) {
+
+				$e->add('Error', $cf7_mail_tag .'->'. $kintone_form_data['code'].' : '.$return_value['message'] . '(' . $return_value['code'] . ')' );
+
+				return;
+			}
+
+			return $return_data;
+		}
+
+	}
+
+}
+
+
