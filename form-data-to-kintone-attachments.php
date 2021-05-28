158689941ce1fa067126b31f1267abc47b058a99
tkc49 <h.tkc49@gmail.com>
2021-05-29 04:10:11 +0900

    some commit

--- form-data-to-kintone-attachments.php
+++ form-data-to-kintone-attachments.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: Form data to kintone Attachments.
- * Plugin URI:  
+ * Plugin URI:
  * Description: This plugin is an addon for "kintone form".
- * Version:	 1.11.0
- * Author:	  Takashi Hosoya
+ * Version:     1.11.0
+ * Author:      Takashi Hosoya
  * Author URI:  http://ht79.info/
- * License:	 GPLv2 
+ * License:     GPLv2
  * Text Domain: kintone-form-attachments
  * Domain Path: /languages
  */
@@ -29,7 +29,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-define( 'KINTONE_FORM_ATTACHMENTS_URL',  plugins_url( '', __FILE__ ) );
+define( 'KINTONE_FORM_ATTACHMENTS_URL', plugins_url( '', __FILE__ ) );
 define( 'KINTONE_FORM_ATTACHMENTS_PATH', dirname( __FILE__ ) );
 
 
@@ -40,54 +40,46 @@ require KINTONE_FORM_ATTACHMENTS_PATH . '/modules/file.php';
 require_once( KINTONE_FORM_ATTACHMENTS_PATH . '/inc/BFIGitHubPluginUploader.php' );
 
 
-
 class KintoneFormAttachments {
 
 	private $version = '';
 	private $langs   = '';
 	private $nonce   = 'kintone_form_attachments_';
-		
-	function __construct()
-	{
-		$data = get_file_data(
-			__FILE__,
-			array( 'ver' => 'Version', 'langs' => 'Domain Path' )
-		);
+
+	function __construct() {
+		$data          = get_file_data( __FILE__,
+			array( 'ver' => 'Version', 'langs' => 'Domain Path' ) );
 		$this->version = $data['ver'];
 		$this->langs   = $data['langs'];
-		
+
 	}
 
-	public function register()
-	{
+	public function register() {
 		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ), 1 );
 	}
 
-	public function plugins_loaded()
-	{
-		load_plugin_textdomain(
-			'kintone-form-attachments',
+	public function plugins_loaded() {
+		load_plugin_textdomain( 'kintone-form-attachments',
 			false,
-			dirname( plugin_basename( __FILE__ ) ).$this->langs
-		);
+			dirname( plugin_basename( __FILE__ ) ) . $this->langs );
 
 		if ( is_admin() ) {
-		    new BFIGitHubPluginUpdater( __FILE__, 'tkc49', "form-data-to-kintone-attachments" );
-		}		
+			new BFIGitHubPluginUpdater( __FILE__, 'tkc49', "form-data-to-kintone-attachments" );
+		}
 
 		add_filter( 'kintone_form_attachments_data', array( $this, 'kintone_form_attachments_data' ), 10, 6 );
 		add_filter( 'kintone_fieldcode_supported_list', array( $this, 'kintone_fieldcode_supported_list' ), 10, 1 );
 
-		
+
 	}
 
-	public function kintone_form_attachments_data( $kintone_setting_data, $appdata, $cf7_send_data, $kintone_form_data, $cf7_mail_tag, $e ){
+	public function kintone_form_attachments_data( $kintone_setting_data, $appdata, $cf7_send_data, $kintone_form_data, $cf7_mail_tag, $e ) {
 
 		return KintoneForm_file::get_kintone_file_key( $kintone_setting_data, $appdata, $cf7_send_data, $kintone_form_data, $cf7_mail_tag, $e );
 
 	}
 
-	public function kintone_fieldcode_supported_list( $kintone_fieldcode_supported_list ){
+	public function kintone_fieldcode_supported_list( $kintone_fieldcode_supported_list ) {
 
 		$kintone_fieldcode_supported_list['FILE'] = 'file';
 
--- modules/file.php
+++ modules/file.php
@@ -1,21 +1,20 @@
-<?php 
+<?php
 
-class KintoneForm_file
-{
+class KintoneForm_file {
 
 	/*
 	 * get instance
 	 */
-	public static function getInstance()
-	{
+	public static function getInstance() {
 		/**
-		* a variable that keeps the sole instance.
-		*/
+		 * a variable that keeps the sole instance.
+		 */
 		static $instance;
 
-		if ( !isset( $instance ) ) {
+		if ( ! isset( $instance ) ) {
 			$instance = new KintoneForm_file();
 		}
+
 		return $instance;
 	}
 
@@ -23,68 +22,56 @@ class KintoneForm_file
 
 		$return_data = array();
 
-		$value = $cf7_send_data[$cf7_mail_tag];
+		$value = $cf7_send_data[ $cf7_mail_tag ];
 
-		if( $kintone_form_data['required'] == 'true' && empty($value) ){
-			$e->add('Error', $cf7_mail_tag .'->'. $kintone_form_data['code'].' : Required fields');
-		}		
+		if ( 'true' === $kintone_form_data['required'] && empty( $value ) ) {
+			$e->add( 'Error', $cf7_mail_tag . '->' . $kintone_form_data['code'] . ' : Required fields' );
+		}
 
 		$submission = WPCF7_Submission::get_instance();
 		if ( empty( $submission ) ) {
-		    return;
-		}		
+			return;
+		}
 		$uploaded_files = $submission->uploaded_files();
 
-		if(!isset($uploaded_files[$cf7_mail_tag])){
+		if ( ! isset( $uploaded_files[ $cf7_mail_tag ] ) ) {
 			return;
 		}
 
-		$filePath = $uploaded_files[$cf7_mail_tag];
-		$fileName = mb_convert_encoding(mb_substr($filePath, mb_strrpos($filePath, DIRECTORY_SEPARATOR) + 1), "UTF-8", "auto");
-		$fileSize = filesize( $filePath );
+		$file_path = $uploaded_files[ $cf7_mail_tag ];
+		$file_name = mb_convert_encoding( mb_substr( $file_path[0], mb_strrpos( $file_path[0], DIRECTORY_SEPARATOR ) + 1 ), 'UTF-8', 'auto' );
 
-		$finfo = finfo_open(FILEINFO_MIME_TYPE);  
-		$mimeType = finfo_file($finfo, $filePath);
-		$file_data = file_get_contents($filePath);
-		finfo_close($finfo);  
-		
-		$request_url = 'https://'.$kintone_setting_data['domain'].'/k/v1/file.json';
-		$headers = array( 
-			'X-Cybozu-API-Token' 	=> $appdata['token'] ,
-		);
+		$finfo     = finfo_open( FILEINFO_MIME_TYPE );
+		$mime_type = finfo_file( $finfo, $file_path[0] );
+		$file_data = file_get_contents( $file_path[0] );
+		finfo_close( $finfo );
 
-		$boundary = '----'.microtime(true);
-		$body = '--'.$boundary."\r\n".
-		        'Content-Disposition: form-data; name="file"; filename="'.$fileName.'"'."\r\n".
-		        'Content-Type: '.$mimeType."\r\n\r\n".
-		        $file_data."\r\n".
-		        '--'.$boundary.'--';
+		$request_url = Kintone_Form_Utility::get_kintone_url( $kintone_setting_data, 'file' );
 
-		$res = wp_remote_post( 
-			$request_url, 
-			[
-		    	'headers' => [
-		    		'Content-Type' => "multipart/form-data; boundary={$boundary}", 
-		    		'X-Cybozu-API-Token' 	=> $appdata['token'],
-		    		'Content-Length' 	=> strlen($body),
-		    	],
-		    	'body' => $body
-			]
+		$boundary = '----' . microtime( true );
+		$body     = '--' . $boundary . "\r\n" . 'Content-Disposition: form-data; name="file"; filename="' . $file_name . '"' . "\r\n" . 'Content-Type: ' . $mime_type . "\r\n\r\n" . $file_data . "\r\n" . '--' . $boundary . '--';
+
+		$res = wp_remote_post(
+			$request_url,
+			array(
+				'headers' => array(
+					'Content-Type'       => "multipart/form-data; boundary={$boundary}",
+					'X-Cybozu-API-Token' => $appdata['token'],
+					'Content-Length'     => strlen( $body ),
+				),
+				'body'    => $body,
+			)
 		);
 
 		if ( is_wp_error( $res ) ) {
-
-			$e = $res;
 			return $res;
-
 		} else {
-
-			$return_value = json_decode( $res['body'], true );
+			$return_value           = json_decode( $res['body'], true );
 			$return_data['value'][] = $return_value;
-			
+
 			if ( isset( $return_value['message'] ) && isset( $return_value['code'] ) ) {
 
-				$e->add('Error', $cf7_mail_tag .'->'. $kintone_form_data['code'].' : '.$return_value['message'] . '(' . $return_value['code'] . ')' );
+				$e->add( 'Error', $cf7_mail_tag . '->' . $kintone_form_data['code'] . ' : ' . $return_value['message'] . '(' . $return_value['code'] . ')' );
 
 				return;
 			}
